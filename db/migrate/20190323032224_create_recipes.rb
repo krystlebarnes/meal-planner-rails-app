2d968619ff7cb4dbdfb43d6cb495e257a0f28f47
github username <github email address>
2019-03-25 22:08:59 -0600

    added description and calories to Recipe migration

--- db/migrate/20190323032224_create_recipes.rb
+++ db/migrate/20190323032224_create_recipes.rb
@@ -2,9 +2,11 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :name
+      t.text :description
       t.integer :prep_time
       t.integer :cook_time
       t.integer :serving_size
+      t.integer :calories
       t.text :instructions
       t.belongs_to :author, references: :users, foreign_key: { to_table: :users}
 
