444416bba9e6789ada09409c3cb0f81b19c535d5
github username <github email address>
2019-04-07 21:03:33 -0600

    added scope to PlannedMeal model to find all meals between two dates

--- app/models/planned_meal.rb
+++ app/models/planned_meal.rb
@@ -4,6 +4,7 @@ class PlannedMeal < ApplicationRecord
   validates :date, :presence => {:message => "can't be blank"}
   validates :meal_type, :presence => {:message => "can't be blank"}
   validate :valid_meal_type?
+  scope :meal_plan, ->(start_date) { where("date BETWEEN ? AND (? + 14.days)", start_date) if start_date.present? }
 
   def valid_meal_type?
     if self.meal_type != "Breakfast" || "Morning Snack" || "Lunch" || "Afternoon Snack" || "Dinner"
