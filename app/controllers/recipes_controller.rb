031092504e5aa877da42ea9b98290cdc4cd06cff
github username <github email address>
2019-04-21 21:12:39 -0600

    added index, new, create, show, edit, update, set _recipe, and recipe_params methods to Recipe Controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,50 @@
 class RecipesController < ApplicationController
+
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    #how do i also assign the recipe author to the current user?
+    #@recipe.author = current_user
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    #how do I make sure a recipe can only be edited by it's author?
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    #i probably need to add author_id?
+    params.require(:recipe).permit(:name, :description, :prep_time, :cook_time, :serving_size, :calories, :instructions)
+  end
+
 end
