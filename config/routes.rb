286c449d9f81708d50807f1931b739f663211b73
github username <github email address>
2019-04-21 20:01:41 -0600

    added login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :planned_meals
   resources :recipes
   resources :users
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
