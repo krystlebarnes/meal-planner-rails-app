effce9fa1b4cae6f25ad1d3ca2b646f11c79bd53
github username <github email address>
2019-05-22 10:40:29 -0600

    fixed recipe association for Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :ingredients
-  has_many :recipes, :through => :planned_meals
+  has_many :recipes, :through => :ingredients
   validates :name, :presence => {:message => "can't be blank"}
   validates :name, :uniqueness => {:message => "already exists" }
 end
