58a8e7ac2b4c875f4dd6fbe2d8df1f3d8cc5a6c3
github username <github email address>
2020-08-02 16:47:46 -0600

    added formatting for meal plan

--- app/controllers/planned_meals_controller.rb
+++ app/controllers/planned_meals_controller.rb
@@ -3,9 +3,25 @@ class PlannedMealsController < ApplicationController
   def index
     if @planner = User.find_by_id(params[:user_id])
       @planned_meals = @planner.planned_meals
+      params[:start_date] && !params[:start_date].empty? ? (@start_date = params[:start_date].to_date) : (@start_date = Date.today)
+      @day_2 = @start_date + 1.day
+      @day_3 = @start_date + 2.days
+      @day_4 = @start_date + 3.days
+      @day_5 = @start_date + 4.days
+      @day_6 = @start_date + 5.days
+      @day_7 = @start_date + 6.days
+      @day_8 = @start_date + 7.days
+      @day_9 = @start_date + 8.days
+      @day_10 = @start_date + 9.days
+      @day_11 = @start_date + 10.days
+      @day_12 = @start_date + 11.days
+      @day_13 = @start_date + 12.days
+      @end_date = @start_date + 13.days
+      @planned_meals = @planner.planned_meals.meal_plan(@start_date, @end_date)
     else
-      @planned_meals = PlannedMeal.all
+      redirect_to root_path
     end
+
   end
   
   def new
