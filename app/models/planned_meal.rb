e2f17af1ebe0c92405d4ae3ff20c9d0f997ef4e8
github username <github email address>
2020-08-02 16:48:54 -0600

    fixed scope method

--- app/models/planned_meal.rb
+++ app/models/planned_meal.rb
@@ -3,6 +3,6 @@ class PlannedMeal < ApplicationRecord
   belongs_to :planner, :class_name => "User", :foreign_key => "planner_id"
   validates :date, :presence => {:message => "can't be blank"}
   validates :meal_type, :presence => {:message => "can't be blank"}
-  scope :meal_plan, ->(start_date) { where("date BETWEEN ? AND (? + 13.days)", start_date) if start_date.present? }
+  scope :meal_plan, ->(start_date, end_date) { where("date BETWEEN ? AND ?", start_date, end_date) if start_date.present? }
 
 end
